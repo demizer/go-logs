430d2a333ed35409282fa2a9e1e31e5e21ed1e9c
Jesus Alvarez <jeezusjr@gmail.com>
2013-02-11 00:41:37 -0800

    Fix docs in color.go

--- color.go
+++ color.go
@@ -1,15 +1,20 @@
 // Copyright 2013 The go-logger Authors. All rights reserved.
-// Thi code is MIT licensed. See the LICENSE file for more info.
+// This code is MIT licensed. See the LICENSE file for more info.
+
 package logger
 
 import (
 	"fmt"
 )
 
+// eCode is an ANSI escape code
 type eCode int
 
 // Ansi escape code constants. See
 // http://ascii-table.com/ansi-escape-sequences.php
+
+// Constants for text attributes, the underscores represent attributes that are
+// not supported.
 const (
 	OFF eCode = iota
 	BOLD
@@ -22,6 +27,7 @@ const (
 	CONCEALED
 )
 
+// Constants for text forground coloring.
 const (
 	BLACK eCode = iota + 30
 	RED
@@ -33,6 +39,7 @@ const (
 	WHITE
 )
 
+// Constants for text background coloring.
 const (
 	BG_GREY eCode = iota + 40
 	BG_RED
@@ -44,13 +51,12 @@ const (
 	BG_WHITE
 )
 
-/* AnsiEscape accepts ANSI escape codes and strings to form escape sequences.
-
-   For example, to create a string with a colorized prefix,
-
-	AnsiEscape(BOLD, GREEN, "[DEBUG] ", OFF, "Here is the debug output")
-
-   and a nicely escaped string for terminal output will be returned. */
+// AnsiEscape accepts ANSI escape codes and strings to form escape sequences.
+// For example, to create a string with a colorized prefix,
+//
+//      AnsiEscape(BOLD, GREEN, "[DEBUG] ", OFF, "Here is the debug output")
+//
+// and a nicely escaped string for terminal output will be returned.
 func AnsiEscape(c ...interface{}) (out string) {
 	for _, val := range c {
 		switch t := val.(type) {
