bb44013cef4204063e197a05875d6a97e243c4ad
Jesus Alvarez <jeezusjr@gmail.com>
2013-02-14 15:16:40 -0800

    Refactor logger.go and add tests
    The new logger.go nearly reimplements the log package from the standard
    library.

--- logger_test.go
+++ logger_test.go
@@ -1,9 +1,30 @@
 package logger
 
 import (
+	"bytes"
+	"os"
 	"testing"
 )
 
-func TestColor(t *testing.T) {
-	// Debug("Testing debug output")
+func TestLevel(t *testing.T) {
+	level := Level()
+	if level != WARNING {
+		t.Errorf("Level() = %s, want %s", level, WARNING)
+	}
+	SetLevel(DEBUG)
+	level = Level()
+	if level != DEBUG {
+		t.Errorf("Level() = %s, want %s", level, DEBUG)
+	}
+}
+
+func TestStream(t *testing.T) {
+	if out := Stream(); out != os.Stderr {
+		t.Errorf("Output stream is not stderr by default")
+	}
+	var buf bytes.Buffer
+	SetStream(&buf)
+	if out := Stream(); out != &buf {
+		t.Errorf("SetOutput(&buf) = %p, want %p", out, &buf)
+	}
 }
