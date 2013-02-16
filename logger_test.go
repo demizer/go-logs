ddc9bffc9a3c07818f0526fa1aff20496736c989
Jesus Alvarez <jeezusjr@gmail.com>
2013-02-15 23:47:02 -0800

    Add test for Output()

--- logger_test.go
+++ logger_test.go
@@ -28,3 +28,41 @@ func TestStream(t *testing.T) {
 		t.Errorf("SetOutput(&buf) = %p, want %p", out, &buf)
 	}
 }
+
+var (
+	boldPrefix  = AnsiEscape(BOLD, "TEST>", OFF)
+	colorPrefix = AnsiEscape(BOLD, RED, "TEST>", OFF)
+	date        = "Mon Jan 02 15:04 2006"
+	badDate     = "on Jan _2 1504:05 006"
+)
+
+var printTests = []struct {
+	template   string
+	prefix     string
+	dateFormat string
+	flags      int
+	text       string
+	want       string
+	wantErr    bool
+}{
+	{logFormat, boldPrefix, date, LstdFlags, "test number 1",
+		"\x1b[1mTEST>\x1b[0m: Mon Jan 02 15:04 2006: test number 1",
+		false},
+	{logFormat, colorPrefix, date, Ldate, "test number 2",
+		"\x1b[1m\x1b[31mTEST>\x1b[0m: Mon Jan 02 15:04 2006: test number 2",
+		false},
+}
+
+func TestOutput(t *testing.T) {
+	var buf bytes.Buffer
+	for i, k := range printTests {
+		log := New(&buf, k.prefix, k.dateFormat, k.template, DEBUG,
+			k.flags)
+		_, err := log.Output(1, k.text, os.Stdout)
+		if buf.String() != k.want || err != nil {
+			t.Errorf("Print test %d failed, got \"%s\" want "+
+				"\"%s\"", i, buf.String(), k.want)
+		}
+		log = nil
+	}
+}
